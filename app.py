a3e949afd94a13a266661ed0dd4e28dbc9dca972
kid873 <ycym4225@gmail.com>
2024-10-30 10:07:59 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import streamlit as st
-import Home
-import first
-import second
+import 어서와
+import 날봐
+import 여기를클릭하세요
 
 def app():
-    st.title("Home")
-    st.write("Welcome to the Home page!")
+    st.title("어서와!")
+    st.write("이런 곳은 처음이지?ㅋㅋ")
 
-page = st.sidebar.selectbox("Select a page", ["Home","first","second"])
+page = st.sidebar.selectbox("Select a page", ["어서와!","날 봐!","여기를 클릭하세요"])
 
-if page == "어디봐?":
+if page == "어서와!":
     Home.app()
 elif page =="날 봐!":
     first.app()
